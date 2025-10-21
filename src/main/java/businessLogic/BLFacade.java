6ad90991dcbd2ff9a19cdfa75ce5c5615b03f9b3
ehu <ehu@158.227.124.11>
2025-10-21 11:14:45 +0200

    Testttt

--- src/main/java/businessLogic/BLFacade.java
+++ src/main/java/businessLogic/BLFacade.java
@@ -99,8 +99,6 @@ public interface BLFacade {
 
 	public Traveler getTraveler(String erab);
 
-	//public Admin getAdmin(String erab);
-
 	public String getMotaByUsername(String erab);
 
 	public boolean addUser(String username, String password, Class<?> userType);
